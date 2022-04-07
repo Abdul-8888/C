a3136312433f19087d7048f7169b024fa3d284da
Abdul-8888 <101495946+Abdul-8888@users.noreply.github.com>
2022-04-07 12:02:47 +0500

    Add files via upload

--- task1.cpp
+++ task1.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 using namespace std;
-main()
+main ()
 {
+	string countryName;
+	float ticPrice , discount;
+	cout <<"Enter Country Name . . . ";
+	cin >> countryName;
 
-cout <<"*******************************************************************************"<<endl
-       <<"*   		University  Admission  Management  System	              *"<<endl	
-       <<"*******************************************************************************"<<endl;
+	cout <<"Enter Ticket Price . . . ";
+	cin >> ticPrice;
 
-
-
+	if (countryName == "Ireland")
+	{	discount = ticPrice*5/100;
+		ticPrice = ticPrice - discount;
+		cout << "Discounted Price is . . . "<<ticPrice;
+	}
+	else
+	{  	discount = ticPrice*10/100;
+		ticPrice = ticPrice - discount;
+		cout << "Discounted Price is . . . "<<ticPrice;
+	}
+		
 
 
 }
--- task2.cpp
+++ task2.cpp
@@ -1,25 +1,23 @@
 #include <iostream>
 using namespace std;
-main()
+main ()
 {
+	
+	float speed;
+	
 
-cout <<"*******************************************************************************"<<endl
-       <<"*   		University  Admission  Management  System	              *"<<endl	
-       <<"*******************************************************************************"<<endl
-       <<endl
-       <<"Main  Menu   >"<<endl
-       <<"______________________________________________________"<<endl
-       <<"Select one of the following option number. . . . ."<<endl
-       <<"1 .  Add New Student Record"<<endl
-       <<"2 .  View all Records"<<endl
-       <<"3 .  Print the Record in Merit Order "<<endl
-       <<"4 .  Generate Merit List"<<endl
-       <<"5 .  Program Seat Management"<<endl
-       <<"6 .  Exit"<<endl
-       <<"Your OPtion . . . ."<<endl;
+	cout <<"Enter Speed . . . ";
+	cin >> speed;
 
-
-
+	if (speed > 100)
+	{	
+		cout << "Halt . . . YOU WILL BE CHALLANED";
+	}
+	else
+	{  	
+		cout << "Perfect! YOU're going good. ";
+	}
+		
 
 
 }
